69e5b5dd467c1da5e777a893cd7b061cbf004f0d
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-01-24 13:11:01 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Creating an instance of the class to invoke the non-static method
+Car car1 = new Car();
+car1.accelerate();
+car1.slow();
+
+        System.out.println(car1.BRAND);
+        System.out.println(car1.model);
+        System.out.println(car1.power);
+
+        // Invoking a static method directly
+        Car.slow();
     }
 }
